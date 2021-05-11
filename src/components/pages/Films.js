90132b6a923d1575eed4aebf7c13c8552520466a
jiewenc <angelachanofficial@gmail.com>
2021-05-10 21:24:29 -0400

    codes for submission

--- src/components/pages/Films.js
+++ src/components/pages/Films.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Project from '../layouts/Project'
 import { projects, miscellaneous, section3Title, section4Title } from '../../profile'
+import WSLimage from '../../img/wsl.jpeg';
+import Film2 from '../../img/debut.png';
+import Film3 from '../../img/1999.png';
+import Film4 from '../../img/drone.png';
+import Film5 from '../../img/tamsui.png';
+import Film6 from '../../img/reflections.png';
 
 const Films = () => {
     return (
         <>
 
-            <div className="third">
+            <div id="Projects" className="third">
                 {/*
                 <div className="row">
                     {miscellaneous && miscellaneous.map((x) => 
@@ -14,12 +20,120 @@ const Films = () => {
                     )}
                 </div>
                 */}
-                <div className="row type1">
-                    <img className="film1" />
+                <div className="film-card">
+                    <div className="film-image-container" >
+                        <img src={WSLimage} alt="where star lies" className="film-image"/>
+                    </div>
                     <div className="film-title">
+                        <div style={{"display": "flex", "flexDirection": "col"}}>
+                            <p className="film-year">2019</p>
+                            <div>
+                                <h2 className="film-name">Where Star Lies</h2>
+                                <p className="film-des">21'59''</p>
+                                <p className="film-des">Drama/Romance</p>
+                            </div>
+                            
+                        </div>
                         
                     </div>
                 </div>
+
+                <div className="film-card">
+                <div className="film-title">
+                        <div style={{"display": "flex", "flexDirection": "col"}}>
+                            <p className="film-year">2019</p>
+                            <div>
+                                <h2 className="film-name">Debut</h2>
+                                <p className="film-des">17' 56''</p>
+                                <p className="film-des">Drama/Costume/Family</p>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    <div className="film-image-container" >
+                        <img src={Film2} alt="where star lies" className="film-image"/>
+                    </div>
+                    
+                </div>
+
+                <div className="film-card">
+                    <div className="film-image-container" >
+                        <img src={Film3} alt="where star lies" className="film-image"/>
+                    </div>
+                    <div className="film-title">
+                        <div style={{"display": "flex", "flexDirection": "col"}}>
+                            <p className="film-year">2017</p>
+                            <div>
+                                <h2 className="film-name">1999</h2>
+                                <p className="film-des">6' 36''</p>
+                                <p className="film-des">Drama/Romance</p>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="film-card">
+
+                <div className="film-title">
+                        <div style={{"display": "flex", "flexDirection": "col"}}>
+                            <p className="film-year">2018</p>
+                            <div>
+                                <h2 className="film-name">Eyes to see Taiwan</h2>
+                                <p className="film-des">15' 30''</p>
+                                <p className="film-des">Documentary</p>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    <div className="film-image-container" >
+                        <img src={Film4} alt="where star lies" className="film-image"/>
+                    </div>
+                    
+                </div>
+
+                <div className="film-card">
+                    <div className="film-image-container" >
+                        <img src={Film5} alt="where star lies" className="film-image"/>
+                    </div>
+                    <div className="film-title">
+                        <div style={{"display": "flex", "flexDirection": "col"}}>
+                            <p className="film-year">2018</p>
+                            <div>
+                                <h2 className="film-name">Tamsui Memories</h2>
+                                <p className="film-des">15' 30''</p>
+                                <p className="film-des">Documentary</p>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="film-card">
+
+                <div className="film-title">
+                        <div style={{"display": "flex", "flexDirection": "col"}}>
+                            <p className="film-year">2020</p>
+                            <div>
+                                <h2 className="film-name">Reflections</h2>
+                                <p className="film-des">3' 20''</p>
+                                <p className="film-des">Drama/Romance</p>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    <div className="film-image-container" >
+                        <img src={Film6} alt="where star lies" className="film-image"/>
+                    </div>
+                    
+                </div>
+
+                
             </div>
         </>
     )
--- src/profile.js
+++ src/profile.js
@@ -1,28 +1,20 @@
-// Change website animations
 const animation = {
-    // make it false to switch off fade-up animation
     animate: true,
-    // animation playing duration
     duration: 750,
-    // if false, animation plays everytime element comes on screen
     once: true
 }
-// Change your display name on tha landing display
 const header = {
     name: "Angela Chen",
 }
 const background = {
-    // Options: Snow or Particle
     type: "Particle"
 }
-// Write a para about yourself here
-// To update your image, go to './styles/images.css'
+
 const section2title = "About Me"
 const about = {
     paragraph: "Angela is a front-end developer, creative programmer, designer, storyteller @CMU who has a huge passion for the combination of technology, design and art."
 }
-// Edit your skill and the percentage you know about it
-// To Add a skill, copy any one below and paste it after the last comma
+
 const skillsBar = [
     {
         name: "HTML5",
@@ -62,13 +54,10 @@ const skillsBar = [
         faClass: "fab fa-wordpress"
     }
 ]
-// Edit your projects, its name, your skills used to make it, and the url. 
-// You can omit freely anything if you dont have it
-// To Add a Project, copy any one below and paste it after the last comma and increment the id's project number
+
 const section3Title = "Recent Projects"
 const projects = [
     {
-        // Add image in './styles/images.css' in #project1
         id: "project1",
         name: "Museum.Live",
         skills: ["Web Application Development"],
@@ -76,7 +65,6 @@ const projects = [
         year: "2020"
     },
     {
-        // Add image in './styles/images.css' in #project2
         id: "project2",
         name: "CMU CS Academy",
         skills: ["Web UX Design"],
@@ -84,7 +72,6 @@ const projects = [
         year: "2020"
     },
     {
-        // Add image in './styles/images.css' in #project3
         id: "project3",
         name: "Foresight",
         skills: ["VR Creative Solution"],
@@ -92,7 +79,6 @@ const projects = [
         year: "2020"
     },
     {
-        // Add image in './styles/images.css' in #project4
         id: "project4",
         name: "Natours",
         skills: ["Front End Development"],
@@ -100,7 +86,6 @@ const projects = [
         year: "2021"
     },
     {
-        // Add image in './styles/images.css' in #project5
         id: "project5",
         name: "Bubble Sort",
         skills: ["Full Stack Development"],
@@ -108,7 +93,6 @@ const projects = [
         year: "2020"
     },
     {
-        // Add image in './styles/images.css' in #project6
         id: "project6",
         name: "Baby Go",
         skills: ["VR Experience Design"],
@@ -116,7 +100,6 @@ const projects = [
         year: "2019"
     },
     {
-        // Add image in './styles/images.css' in #project6
         id: "project7",
         name: "Equinox",
         skills: ["VR Experience Design"],
@@ -124,7 +107,6 @@ const projects = [
         year: "2019"
     },
     {
-        // Add image in './styles/images.css' in #project6
         id: "project8",
         name: "Tunnel Hack",
         skills: ["VR Experience Design"],
@@ -132,47 +114,21 @@ const projects = [
         year: "2019"
     },
 ]
-// Edit your Miscellaneous Activities, its name and the url. 
-// You can omit freely anything if you dont have it
-// To Add a Activity, copy any one below and paste it after the last comma and increment the id's Miscellaneous number
-const section4Title = "Miscellaneous"
-const miscellaneous = [
-    {
-        // Add image in './styles/images.css' in #misc1
-        id: "misc1",
-        name: "Miscellaneous 1",
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #misc2        
-        id: "misc2",
-        name: "Miscellaneous 2",
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #misc3
-        id: "misc3",
-        name: "Miscellaneous 3",
-        url: "https://github.com/kaustubhai"
-    },
-]
-// Contact form text, and Jotforms link
-// To get your own jotform link, go to https://www.jotform.com/
-// If you hacve the link already, paste it in the contactUrl below
+
 const section5Title = "Get in Touch"
 const contact = {
     pitch: "Let's talk and make create projects together! Feel free to connect with me through Linkedin, Facebook, Instagram or Github :)",
     copyright: "Jiewen (Angela) Chen",
     contactUrl: ''
 }
-// Paste your respective social media links. You can omit any if you dont have it
-// Upload your resume in your drive, get the shaareable link and paste it in the resume section
+
 const social = {
-    github: 'https://github.com',
+    github: 'https://github.com/angelachan1997/',
     facebook: 'https://facebook.com',
     linkedin: 'https://linkedin.com',
     instagram: 'https://instagram.com',
-    resume: 'https://novoresume.com/'
+    resume: '#'
 }
-// Dont change anything here
-export { animation, header, background, about, skillsBar, projects, miscellaneous, contact, social, section2title, section3Title, section4Title, section5Title }
+
+
+export { animation, header, background, about, skillsBar, projects, contact, social, section2title, section3Title, section5Title }
